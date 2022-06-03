b449d02a86f47ad954ab439b6608743ec37577aa
AlfonsoR1 <alfonsorocha101@gmail.com>
2022-06-02 20:57:08 -0500

    First code done in C++

--- src/examples/02_expressions/06_multi_assign/main.cpp
+++ src/examples/02_expressions/06_multi_assign/main.cpp
@@ -1,6 +1,22 @@
 
+#include<iostream>
+#include "multi_assign.h"
+
+using std::cin; using std::cout;
+
+
 int main()
 {
+    auto num = 0;
+    cout<<"Enter a number:";
+    cin>>num;
+
+    auto result = multiply_numbers(num);
+    cout<<"Result is"<<result<<"\n";
+
+    auto num_1 = 4;
+    result = multiply_numbers(num_1);
+    cout<<"Result is"<<result<<"\n";
 
     return 0;
 }
