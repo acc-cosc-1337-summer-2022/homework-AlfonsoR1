acd474e9c879ab65760e41a19ec3818b55a8a067
AlfonsoR1 <alfonsorocha101@gmail.com>
2022-06-19 13:23:08 -0500

    late HW

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,14 @@
 //write include required statements
-
+#include<string>
 
 //Write a function(s) prototype  here
 
 
+// 1) A get_grade_points function that accepts a string (letter_grade) and returns the grade points.
+char get_grade_points (char letter_grade);
+
+//2) A calculate_gpa function that accepts credit_hours and credit_points parameter and
+// returns their quotient (credit_points/credit_hours).
 
 
+double calculate_gpa (int credit_hours, double credit_points);
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,36 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
 
 int main() 
 {
+	char letter_grade;
+	auto choice = 'n';
+	auto credit_point = 0;
+	auto credit_hours = 0;
+
+	cout<<"Do you want to continue? y or n:";
+	cin>>choice;
+
+	while (choice != 0 ))
+	{
+	//cout<<"Enter the credit hour:";
+	//cin>>credit_hours;
+	credit_point = get_grade_points(letter_grade);
+	cout<<"Enter letter grade:";
+	cin>>letter_grade;
+	credit_point =+ credit_point;
+	cout<<credit_point<<"\n";
+	}
+
+
+
+
+
+
 	return 0;
 }
