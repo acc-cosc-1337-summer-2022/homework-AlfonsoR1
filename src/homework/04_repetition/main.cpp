123db51d04994ad2f9bdaec80807facd738d8cc6
AlfonsoR1 <alfonsorocha101@gmail.com>
2022-06-24 16:42:40 -0500

    Finished

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
-
+#include"dna.h"
+#include <iostream>
 //write using statements
-
+using std::cin;
+using std::cout;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +13,39 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
+
 int main() 
 {
+	char option = 'N';
+	do
+	{
+   	string user_decision;
+	cout <<"------MAIN MENU------"<<"\n";
+    cout << "Please enter 1 for Get GC Content"<<"\n";
+    cout << "Please enter 2 for Get DNA Complement"<<"\n";
+	cin>>user_decision;
+	cout <<"You have entered:"<< user_decision<<"\n";
+
+	string user_dna;
+	cout<<"Please enter a dna string:"<<'\n';
+	cin>>user_dna;
+	cout <<"You have entered:"<< user_dna<<"\n";
+	
+	if (user_decision[0] == '1')
+	{
+		double GC = get_gc_content(user_dna);
+		cout<<"The total amount of G and C is "<<GC<<"\n";
+	}
+	if (user_decision[0] == '2')
+	{
+		string reverse_opposite = get_dna_complement(user_dna);
+		cout<<"You DNA compliment is "<<reverse_opposite<<"\n";
+	}
+
+	cout << "Do you want to continue Y or N"<<"\n";
+	cin>>option;
+	}
+	while (option == 'Y' || option == 'y');
+
 	return 0;
 }
