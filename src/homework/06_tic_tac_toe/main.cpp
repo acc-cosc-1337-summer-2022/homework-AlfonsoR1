c8a87a6d575d567308ad4a959a78d580558f9c7d
AlfonsoR1 <alfonsorocha101@gmail.com>
2022-06-30 21:26:57 -0500

    HW completed

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,33 @@
+#include"tic_tac_toe.h"
+#include<iostream>
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	TicTacToe game;
+	string first_player;
+	char user_choice = 'N';
+	do
+	{
+		cout<<"Enter first player: ";
+		cin>>first_player;
 
+		game.start_game(first_player);
+		int position;
+
+		while(!game.game_over())
+		{
+			cout<<"Enter a position: ";
+			cin>>position;
+			game.mark_board(position); 
+			game.display_board();
+		}
+
+		cout<<"Play Again Y or N: ";
+		cin>> user_choice;
+
+
+	}while(user_choice == 'Y' || user_choice == 'y');
 	return 0;
 }
