98e4f4774cb79c80a082d5e454a5294ca658eb1c
AlfonsoR1 <alfonsorocha101@gmail.com>
2022-07-13 17:19:43 -0500

    done

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -3,6 +3,7 @@
 
 using std::cout;
 
+
 bool TicTacToe :: game_over()
 {
     if(check_row_win() || check_column_win() || check_diagonal_win())
@@ -31,14 +32,6 @@ void TicTacToe :: mark_board(int position)
     set_next_player();
 }
 
-void TicTacToe :: display_board()const
-{
-    for(long unsigned int i=0; i < pegs.size(); i+=3)
-    {
-        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
-    }
-
-}
 
 // Private functions 
 
@@ -72,52 +65,6 @@ bool TicTacToe :: check_board_full()
     }
 
     return true;
-}
-bool TicTacToe:: check_column_win()
-{
-    if(pegs[0]!=" " && pegs[0]==pegs[3] && pegs[3]==pegs[6] )
-    {
-        return true;
-    }
-    if(pegs[1]!=" " && pegs[1]==pegs[4] && pegs[4]==pegs[7] )
-    {
-        return true;
-    }
-    if(pegs[2]!=" " &&pegs[2]==pegs[5] && pegs[5]==pegs[8] )
-    {
-        return true;
-    }
-
-    return false;
-}
-bool  TicTacToe:: check_row_win()
-{
-    if( pegs[0]!=" " && pegs[0]==pegs[1] && pegs[1]==pegs[2] )
-    {
-        return true;
-    }
-    if( pegs[3]!=" " &&pegs[3]==pegs[4] && pegs[4]==pegs[5] )
-    {
-        return true;
-    }
-    if( pegs[6]!=" " &&pegs[6]==pegs[7] && pegs[7]==pegs[8] )
-    {
-        return true;
-    }
-
-    return false;
-}
-bool TicTacToe:: check_diagonal_win()
-{
-    if( pegs[0]!=" " &&pegs[0]==pegs[4] && pegs[4]==pegs[8] )
-    {
-        return true;
-    }
-    if( pegs[2]!=" " &&pegs[2]==pegs[4] && pegs[4]==pegs[6] )
-    {
-        return true;
-    }
-    return false;
 }
 void TicTacToe:: set_winner()
 {
